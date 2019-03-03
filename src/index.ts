985220ede7b18b9304b9e37dc570c0903084b18b
Anthony Naddeo <anthony.naddeo+wls-bionic@gmail.com>
2019-03-02 23:03:17 -0800

    Saving after the demo

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,47 @@
-import ReactDOM from 'react-dom'
-import React from 'react'
-import { App } from './App'
+import Immutable from 'immutable'
 
-const div = document.getElementById('root')
 
-if (div) {
-    ReactDOM.render(React.createElement(App), div)
-} else {
-    throw Error('Cannot find element with id root')
+const PersonRecordCreator = Immutable.Record({
+    firstName: '',
+    lastName: '',
+    address: ['WA', '98102'] as const
+})
+
+
+
+
+
+/**
+  Our library code
+  */
+
+type State = 'WA' | 'NY' | 'ME'
+
+
+type Zip = '98102' | '10803' | '12222'
+
+interface Person {
+    readonly firstName: string
+    readonly lastName: string
+    readonly address: readonly [State, Zip]
 }
+
+
+function doStuff(person: Immutable.RecordOf<Person>){
+    console.log(person.firstName)
+}
+
+
+
+
+
+/**
+ * Using the library
+ */
+const myPerson = PersonRecordCreator({
+    firstName: 'bar',
+    lastName: 'foobar'
+})
+
+doStuff(myPerson)
+
